9e528e3f960da0797722187201da39637f6808e4
urigross <urigross@gmail.com>
2021-07-14 21:21:47 +0300

    continue home-page design

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,23 +1,34 @@
 <template>
-    <div class="home-top">
-        <main class="home-search">
-                <header class="home-header">
-                    <h1><span>You need a <i>free lance</i> in a free world</span></h1>
-                </header>
-                <input class="home-search" >
-                <button>Search</button>
-                <div class="home-tag-buttons">
-                    <span>Popular</span>
-                    <button>Web Design</button>
-                    <button>WordPress</button>
-                    <button>Logo Design</button>
-                    <button>Dropshipping</button>
+    <div class="home-page">
+        <div class="home-page-top-container">
+            <div class="home-page-top layout">
+                <main class="home-page-search">
+                        <header class="header">
+                            <h1><span>You need a <i>free lance</i> in a free world</span></h1>
+                        </header>
+                        <form class="home-page-search-container">
+                            <div class="home-page-search-bar-container">
+                                <!-- <span class="search-bar-icon" style="width: 16px; height: 16px;"><svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M15.8906 14.6531L12.0969 10.8594C12.025 10.7875 11.9313 10.75 11.8313 10.75H11.4187C12.4031 9.60938 13 8.125 13 6.5C13 2.90937 10.0906 0 6.5 0C2.90937 0 0 2.90937 0 6.5C0 10.0906 2.90937 13 6.5 13C8.125 13 9.60938 12.4031 10.75 11.4187V11.8313C10.75 11.9313 10.7906 12.025 10.8594 12.0969L14.6531 15.8906C14.8 16.0375 15.0375 16.0375 15.1844 15.8906L15.8906 15.1844C16.0375 15.0375 16.0375 14.8 15.8906 14.6531ZM6.5 11.5C3.7375 11.5 1.5 9.2625 1.5 6.5C1.5 3.7375 3.7375 1.5 6.5 1.5C9.2625 1.5 11.5 3.7375 11.5 6.5C11.5 9.2625 9.2625 11.5 6.5 11.5Z"></path></svg></span> -->
+                                <input class="home-page-search-bar" placeholder='Try: "building mobile app"' autocomplete="off">
+                                <button>Search</button>
+                            </div>
+                        </form>
+                        <div class="home-page-tag-buttons">Popular:
+                            <button>Web Design</button>
+                            <button>WordPress</button>
+                            <button>Logo Design</button>
+                            <button>Dropshipping</button>
+                        </div>
+                </main>
+                <div class="home-page-image-container">
+                    <picture>
+                        <img src="https://st.depositphotos.com/1008939/i/600/depositphotos_18807295-stock-photo-portrait-of-handsome-man.jpg">
+                    </picture>
                 </div>
-        </main>
-        <div class="home-image-container">
-            <picture>
-                <img src="https://st.depositphotos.com/1008939/i/600/depositphotos_18807295-stock-photo-portrait-of-handsome-man.jpg">
-            </picture>
+            </div>
+        </div>
+        <div class="home-page-bottom-container layout">
+            <h1>Popular professional services</h1>
         </div>
     </div>
 </template>
