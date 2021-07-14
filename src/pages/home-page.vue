e5eb70547df9e1187a79aa4d75c14e56019515a0
urigross <urigross@gmail.com>
2021-07-14 17:17:53 +0300

    commit after creating home page

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,28 +1,30 @@
 <template>
-  <section class="container home text-center">
-    <h1>Home Page</h1>
-    <h5 class="text-center">
-      Powered up Frontend 
-    </h5>
-    <img alt="Vue logo" src="../assets/logo.png">
-  </section>
-
-  
+    <div class="home-top">
+        <main class="home-search">
+                <header class="home-header">
+                    <h1><span>You need a <i>free lance</i> in a free world</span></h1>
+                </header>
+                <input class="home-search" >
+                <button>Search</button>
+                <div class="home-tag-buttons">
+                    <span>Popular</span>
+                    <button>Web Design</button>
+                    <button>WordPress</button>
+                    <button>Logo Design</button>
+                    <button>Dropshipping</button>
+                </div>
+        </main>
+        <div class="home-image-container">
+            <picture>
+                <img src="https://st.depositphotos.com/1008939/i/600/depositphotos_18807295-stock-photo-portrait-of-handsome-man.jpg">
+            </picture>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'home',
-  data() {
-    return {
-    }
-  },
-  computed: {
-  },
-  created() {
-  },
-  methods: {
-  }
-  
-}
+export default {};
 </script>
+
+<style>
+</style>
