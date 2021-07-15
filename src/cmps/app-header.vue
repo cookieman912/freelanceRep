bc7d5f1d2fb24b043be12b94e70bdb355c7e1c22
Yaniv bar <yanivb527@gmail.com>
2021-07-15 13:49:35 +0300

    day 2 noon commit modals done

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -8,19 +8,22 @@
         <router-link to="/">Home</router-link>
         <router-link to="/explore">Explore</router-link>
         <router-link to="/becomeSeller">Become a seller</router-link>
-       <button @click="toggleLogin">Sign In</button> 
-        <button @click="toggleSignup">
+        <span v-if="loggedInUser">{{loggedInUser.fullname}}</span>
+     
+       <button v-else @click="toggleLogin">Sign In</button> 
+          <span v-if="loggedInUser" @click="signout">sign out</span>
+        <button v-else @click="toggleSignup">
           <span class="header-join">Join</span>
         </button>
        
       </div>
     </nav>
-    <section class="loggedin-user" v-if="loggedInUser">
+    <!-- <section class="loggedin-user" v-if="loggedInUser">
       <router-link :to="`/user/${loggedInUser._id}`">
         {{ loggedInUser.fullname }}
       </router-link>
       <span>{{ loggedInUser.score }}</span>
-    </section>
+    </section> -->
   </header>
 </template>
 <script>
@@ -33,12 +36,15 @@ export default {
     },
   },
   methods:{
-    toggleLogin(){
+     toggleLogin(){
     eventBusService.$emit('toggle-login')
     },
       toggleSignup(){
     eventBusService.$emit('toggle-signup')
-    }
+    },
+     async signout(){
+        await this.$store.dispatch({ type: "logout" });
+     }
   }
 };
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,6 +5,10 @@ const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
 const SCORE_FOR_REVIEW = 10
 var gWatchedUser = null;
 
+
+
+
+
 export const userService = {
     login,
     logout,
@@ -18,31 +22,32 @@ export const userService = {
 }
 
 window.userService = userService
-// Note: due to async, must run one by one...
-// userService.signup({fullname: 'Puki Norma', username: 'user1', password:'123',score: 100, isAdmin: false})
-// userService.signup({fullname: 'Master Adminov', username: 'admin', password:'123', score: 100, isAdmin: true})
-// userService.signup({fullname: 'Muki G', username: 'muki', password:'123', score: 100})
+    // Note: due to async, must run one by one...
+    // userService.signup({ fullname: 'Puki Norma', username: 'user1', password: '123', score: 100, isAdmin: false })
+    // userService.signup({ fullname: 'Master Adminov', username: 'admin', password: '123', score: 100, isAdmin: true })
+    // userService.signup({ fullname: 'Muki G', username: 'muki', password: '123', score: 100 })
 
 function getUsers() {
     return storageService.query('user')
-    // return httpService.get(`user`)
+        // return httpService.get(`user`)
 }
 
 async function getById(userId) {
     const user = await storageService.get('user', userId)
-    // const user = await httpService.get(`user/${userId}`)
+        // const user = await httpService.get(`user/${userId}`)
     gWatchedUser = user;
     return user;
 }
+
 function remove(userId) {
     return storageService.remove('user', userId)
-    // return httpService.delete(`user/${userId}`)
+        // return httpService.delete(`user/${userId}`)
 }
 
 async function update(user) {
     await storageService.put('user', user)
-    // user = await httpService.put(`user/${user._id}`, user)
-    // Handle case in which admin updates other user's details
+        // user = await httpService.put(`user/${user._id}`, user)
+        // Handle case in which admin updates other user's details
     if (getLoggedinUser()._id === user._id) _saveLocalUser(user)
     return user;
 }
@@ -50,23 +55,25 @@ async function update(user) {
 async function login(userCred) {
     const users = await storageService.query('user')
     const user = users.find(user => user.username === userCred.username)
-    return _saveLocalUser(user)
-
-    // const user = await httpService.post('auth/login', userCred)
-    // socketService.emit('login', user._id);
-    // if (user) return _saveLocalUser(user)
+    if (user) {
+        console.log('welcome!', user.username)
+        return _saveLocalUser(user)
+    } else throw 'wrong creds'
+        // const user = await httpService.post('auth/login', userCred)
+        // socketService.emit('login', user._id);
+        // if (user) return _saveLocalUser(user)
 }
 async function signup(userCred) {
     userCred.score = 100;
     const user = await storageService.post('user', userCred)
-    // const user = await httpService.post('auth/signup', userCred)
-    // socketService.emit('set-user-socket', user._id);
+        // const user = await httpService.post('auth/signup', userCred)
+        // socketService.emit('set-user-socket', user._id);
     return _saveLocalUser(user)
 }
 async function logout() {
     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
-    // socketService.emit('unset-user-socket');
-    // return await httpService.post('auth/logout')
+        // socketService.emit('unset-user-socket');
+        // return await httpService.post('auth/logout')
 }
 
 async function increaseScore(by = SCORE_FOR_REVIEW) {
@@ -88,12 +95,12 @@ function getLoggedinUser() {
 
 // This IIFE functions for Dev purposes 
 // It allows testing of real time updates (such as sockets) by listening to storage events
-(async () => {
+(async() => {
     var user = getLoggedinUser()
-    // Dev Helper: Listens to when localStorage changes in OTHER browser
+        // Dev Helper: Listens to when localStorage changes in OTHER browser
 
     // Here we are listening to changes for the watched user (comming from other browsers)
-    window.addEventListener('storage', async () => {
+    window.addEventListener('storage', async() => {
         if (!gWatchedUser) return;
         const freshUsers = await storageService.query('user')
         const watchedUser = freshUsers.find(u => u._id === gWatchedUser._id)
@@ -107,7 +114,7 @@ function getLoggedinUser() {
 })();
 
 // This is relevant when backend is connected
-(async () => {
+(async() => {
     var user = getLoggedinUser()
     if (user) socketService.emit('set-user-socket', user._id)
 })();
