5ec82801127dc857c59914452ff8eb37e0b2f2f4
Dotan Gerbi <dotangerbi1@gmail.com>
2021-07-18 20:51:43 +0300

    sunday 21:00 - Dotan

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,12 @@
 import { storageService } from './async-storage.service.js'
 import { httpService } from './http.service.js'
 import { socketService, SOCKET_EVENT_USER_UPDATED } from './socket.service.js'
+const USER_KEY = 'userDB'
+
+const defaultUsers = require('../../data/user.json')
+
+
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
-const SCORE_FOR_REVIEW = 10
 var gWatchedUser = null;
 
 
@@ -22,74 +26,69 @@ export const userService = {
 }
 
 window.userService = userService
-    // Note: due to async, must run one by one...
-    // userService.signup({ fullname: 'Puki Norma', username: 'user1', password: '123', score: 100, isAdmin: false })
-    // userService.signup({ fullname: 'Master Adminov', username: 'admin', password: '123', score: 100, isAdmin: true })
-    // userService.signup({ fullname: 'Muki G', username: 'muki', password: '123', score: 100 })
+// Note: due to async, must run one by one...
+// userService.signup({ fullname: 'Puki Norma', username: 'user1', password: '123', score: 100, isAdmin: false })
+// userService.signup({ fullname: 'Master Adminov', username: 'admin', password: '123', score: 100, isAdmin: true })
+// userService.signup({ fullname: 'Muki G', username: 'muki', password: '123', score: 100 })
 
 function getUsers() {
-    return storageService.query('user')
-        // return httpService.get(`user`)
+    // return httpService.get(`user`)
+    // return storageService.query('user')
+    return storageService.query(USER_KEY)
+        .then(users => {
+            if (!users.length) {
+                storageService.postMany(USER_KEY, defaultUsers)
+                return defaultUsers
+            }
+            return users;
+        })
 }
 
 async function getById(userId) {
-    const user = await storageService.get('user', userId)
-        // const user = await httpService.get(`user/${userId}`)
-    gWatchedUser = user;
+    const user = await storageService.get(USER_KEY, userId)
+    // const user = await httpService.get(`user/${userId}`)
+    // gWatchedUser = user;
     return user;
 }
 
 function remove(userId) {
-    return storageService.remove('user', userId)
-        // return httpService.delete(`user/${userId}`)
+    return storageService.remove(USER_KEY, userId)
+    // return httpService.delete(`user/${userId}`)
 }
 
 async function update(user) {
-    await storageService.put('user', user)
-        // user = await httpService.put(`user/${user._id}`, user)
-        // Handle case in which admin updates other user's details
+    await storageService.put(USER_KEY, user)
+    // user = await httpService.put(`user/${user._id}`, user)
+    // Handle case in which admin updates other user's details
     if (getLoggedinUser()._id === user._id) _saveLocalUser(user)
     return user;
 }
 
 async function login(userCred) {
-    const users = await storageService.query('user')
-    const user = users.find(user => user.username === userCred.username)
+    // const users = await storageService.query(USER_KEY)
+    const user = defaultUsers.find(user => user.username === userCred.username)
     if (user) {
-
         return _saveLocalUser(user)
     } else throw 'wrong creds'
-        // const user = await httpService.post('auth/login', userCred)
-        // socketService.emit('login', user._id);
-        // if (user) return _saveLocalUser(user)
+    // const user = await httpService.post('auth/login', userCred)
+    // socketService.emit('login', user._id);
+    // if (user) return _saveLocalUser(user)
 }
 async function signup(userCred) {
     console.log(userCred)
     userCred.fullname = userCred.username;
     userCred.imgUrl = "../src/assets/images/users/default.png"
     const user = await storageService.post('user', userCred)
-        // const user = await httpService.post('auth/signup', userCred)
-        // socketService.emit('set-user-socket', user._id);
+    // const user = await httpService.post('auth/signup', userCred)
+    // socketService.emit('set-user-socket', user._id);
     return _saveLocalUser(user)
 }
 async function logout() {
     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
-        // socketService.emit('unset-user-socket');
-        // return await httpService.post('auth/logout')
+    // socketService.emit('unset-user-socket');
+    // return await httpService.post('auth/logout')
 }
 
-async function increaseScore(by = SCORE_FOR_REVIEW) {
-    const user = getLoggedinUser()
-    user.score = user.score + by || by
-    await update(user)
-    return user.score
-}
-
-
-function _saveLocalUser(user) {
-    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
-    return user
-}
 
 function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER) || 'null')
@@ -97,12 +96,12 @@ function getLoggedinUser() {
 
 // This IIFE functions for Dev purposes 
 // It allows testing of real time updates (such as sockets) by listening to storage events
-(async() => {
+(async () => {
     var user = getLoggedinUser()
-        // Dev Helper: Listens to when localStorage changes in OTHER browser
+    // Dev Helper: Listens to when localStorage changes in OTHER browser
 
     // Here we are listening to changes for the watched user (comming from other browsers)
-    window.addEventListener('storage', async() => {
+    window.addEventListener('storage', async () => {
         if (!gWatchedUser) return;
         const freshUsers = await storageService.query('user')
         const watchedUser = freshUsers.find(u => u._id === gWatchedUser._id)
@@ -116,7 +115,12 @@ function getLoggedinUser() {
 })();
 
 // This is relevant when backend is connected
-(async() => {
+(async () => {
     var user = getLoggedinUser()
     if (user) socketService.emit('set-user-socket', user._id)
 })();
+
+function _saveLocalUser(user) {
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
+    return user
+}
