28da2c6c363885d4f1cb369b33a545d15f3a9db9
Dotan-G <dotangerbi1@gmail.com>
2021-07-24 21:04:03 +0300

    saturdat night

--- src/cmps/gigs-list.vue
+++ src/cmps/gigs-list.vue
@@ -1,35 +1,34 @@
 <template>
-  <section class="gigs-list" v-bind:style="styleObject">
-    <gig-preview v-for="gig in gigs" :gig="gig" :key="gig._id" />
-  </section>
+    <section class="gigs-list" v-bind:style="styleObject">
+        <gig-preview v-for="gig in gigs" :gig="gig" :key="gig._id" />
+    </section>
 </template>
 
 <script>
 import gigPreview from "../cmps/gig-preview.vue";
 
 export default {
-  components: {
-    gigPreview,
-  },
-  props: {
-    gigs: Array,
-  },
-  data() {
-    return {
-      styleObject:{
-        gridTemplateColumns: null,
-      }
-    };
-  },
-  methods: {},
-  computed: {},
-  created() {
-    if(this.$route.name === 'home'){
-     this.styleObject.gridTemplateColumns ='repeat(5, 1fr)';
-   }
-    
-  },
-  destroyed() {},
+    components: {
+        gigPreview,
+    },
+    props: {
+        gigs: Array,
+    },
+    data() {
+        return {
+            styleObject: {
+                gridTemplateColumns: null,
+            },
+        };
+    },
+    methods: {},
+    computed: {},
+    created() {
+        if (this.$route.name === "home") {
+            this.styleObject.gridTemplateColumns = "repeat(5, 1fr)";
+        }
+    },
+    destroyed() {},
 };
 </script>
 
--- src/pages/explore.vue
+++ src/pages/explore.vue
@@ -1,57 +1,56 @@
 <template>
-  <main class="explore" v-if="gigs">
-    <h2 v-if="searchTxt" >Results for " {{ searchTxt }} "</h2>
-    <h2>{{searchTags}}</h2>
-    <filter-by @filter="filter" />
-    <gigs-list :gigs="getGigs" />
-    <!-- <p>{{gigs}}</p> -->
-  </main>
+    <main class="explore main-layout" v-if="gigs">
+        <h2 v-if="searchTxt">Results for " {{ searchTxt }} "</h2>
+        <h2 v-if="searchTags">{{ searchTags }}</h2>
+        <filter-by @filter="filter" />
+        <gigs-list :gigs="getGigs" />
+        <!-- <p>{{gigs}}</p> -->
+    </main>
 </template>
 
 <script>
 import gigsList from "../cmps/gigs-list.vue";
 import filterBy from "../cmps/explore-filter.vue";
 export default {
-  components: {
-    gigsList,
-    filterBy,
-  },
-  data() {
-    return {
-      gigs: [],
-      searchTxt: '',
-      searchTags: ''
-    };
-  },
-  methods: {
-    async filter(filterBy) {
-      try {
-        this.searchTxt = filterBy.txt
-        this.searchTags = filterBy.tags
-        this.$store.commit({ type: "setFilter", filterBy });
-        await this.$store.dispatch("loadGigs")
-        console.log("filterBy", filterBy);
-      } catch (err) {
-        console.log("cannot load gigs", err);
-        throw err;
-      }
+    components: {
+        gigsList,
+        filterBy,
     },
-  },
-  computed: {
-    getGigs() {
-      this.gigs = this.$store.getters.gigsToShow
-      return this.$store.getters.gigsToShow;
+    data() {
+        return {
+            gigs: [],
+            searchTxt: "",
+            searchTags: "",
+        };
     },
-
-  },
-  async created() {
-    try {
-      this.$store.dispatch({ type: "loadGigs" });
-    } catch (err) {
-      console.log("cannot load gigs", err);
-      throw err;
-    }
-  },
-  destroyed() {},
+    methods: {
+        async filter(filterBy) {
+            try {
+                this.searchTxt = filterBy.txt;
+                this.searchTags = filterBy.tags;
+                this.$store.commit({ type: "setFilter", filterBy });
+                await this.$store.dispatch("loadGigs");
+                console.log("filterBy", filterBy);
+            } catch (err) {
+                console.log("cannot load gigs", err);
+                throw err;
+            }
+        },
+    },
+    computed: {
+        getGigs() {
+            this.gigs = this.$store.getters.gigsToShow;
+            return this.$store.getters.gigsToShow;
+        },
+    },
+    async created() {
+        try {
+            this.$store.dispatch({ type: "loadGigs" });
+        } catch (err) {
+            console.log("cannot load gigs", err);
+            throw err;
+        }
+    },
+    destroyed() {},
 };
 </script>
